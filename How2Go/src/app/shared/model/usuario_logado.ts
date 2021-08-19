19dc3c932e4801ff9b5ab5c8a6568959e1d56f15
imbruh <brunaluiza112001@gmail.com>
2021-08-18 21:03:55 -0300

    criacao e listagem de postagem

---
+++ How2Go/src/app/shared/model/usuario_logado.ts
@@ -0,0 +1,12 @@
+import { Postagem } from "./postagem"
+
+export const usuarioLogado = [
+    {
+        idUsuario: 1,
+        email: 'admin',
+        senha: 'admin',
+        nome: 'admin',
+        foto: '../../../assets/usuario/default.png',
+        postagens: Array<Postagem>()
+    }
+]
